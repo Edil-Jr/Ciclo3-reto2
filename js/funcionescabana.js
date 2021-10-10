4518826e05d42f5d1dc1cc4a466a1aaf322cacba
mariorodriguez1mt <mario.rodriguez1.mt@correo.usa.edu.co>
2021-10-10 15:32:30 -0500

    Adicion de formulario para registro de nueva cabaña
    Por el momento tiene un error en el importe de la información con el servidor

--- js/funcionescabana.js
+++ js/funcionescabana.js
@@ -1,24 +1,26 @@
 //creacion funcion lectura datos
 $(document).ready(function () {
-    adquirirdatos();
+    adquirirDatos();
 });
 
-function adquirirdatos(){
+function adquirirDatos(){
+    $("#registronuevo").hide();
     $.ajax({
         url: "https://g6b8ff7bc67d349-db202109302119.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/cabin/cabin",
         type: 'GET',
         dataType: 'JSON',
         success: function(answers){
             console.log(answers);
-            desplieguedatos(answers.items);
+            despliegueDatos(answers.items);
         },
         error: function(xhr,status){
-            window.alert("No se logró actualizar la tabla");
+            alert("No se logró actualizar la tabla");
         }
     })
 }
 
-function desplieguedatos(items){
+function despliegueDatos(items){
+    const identificador = [];
     let tablaDatos =`<table class="table table-dark table-striped">
                         <thead>
                         <tr>
@@ -36,8 +38,44 @@ function desplieguedatos(items){
         tablaDatos+="<td>"+items[i].rooms+"</td>";
         tablaDatos+="<td>"+items[i].category_id+"</td>";
         tablaDatos+="<td>"+items[i].name+"</td>";
+        tablaDatos+=`<td><button class="btn btn-primary" type="button">Editar</button>
+        <button class="btn btn-primary" type="button">Borrar</button>
+        </td>`;
         tablaDatos+="</tr>";
     }
     tablaDatos+="<tbody></table>";
     $("#listadatos").html(tablaDatos);
+    $("#listadatos").show(200);
+}
+
+function pagnuevoRegistro(){
+    $("#listadatos").hide();
+    $("#registronuevo").show(500);
+}
+
+function agregarRegistro(){
+    let dataj = {id:$("#idcabana").val(),
+    brand:$("#brandcabana").val(),
+    rooms:$("#roomscabana").val(),
+    category_id:$("#categorycabana").val(),
+    name:$("#namecabana").val()};
+    if(dataj.id=="" || dataj.brand=="" || dataj.rooms=="" || dataj.category_id=="" || dataj.name==""){
+        alert("Ingrese valores en todos los campos");
+    }
+    else{
+        let datatoSend = JSON.stringify(dataj);
+        $.ajax({
+        url: "https://g6b8ff7bc67d349-db202109302119.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/cabin/cabin",
+        type: 'POST',
+        data: datatoSend,
+        dataType: 'JSON',
+        success: function(answers){
+            console.log(answers);
+            alert("Información cargada con exito");
+        },
+        error: function(xhr,status){
+            alert("No se logró cargar la información");
+        }
+        })
+    }
 }
